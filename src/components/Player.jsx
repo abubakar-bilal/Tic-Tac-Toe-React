09a29f99a839de596288aa532b18ef8aeb7cc38b
Abubaker Bilal <abubaker031@gmail.com>
2024-05-08 17:08:51 +0500

    Tic tac toe game completed with deep react knowledge

---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let playerNameContainer = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    playerNameContainer = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {playerNameContainer}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
